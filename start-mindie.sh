500a198d77260f1047d570f39849b084499a659b
yyhhenry <yyhhenry@foxmail.com>
2025-08-03 14:56:30 +0800

    chore: update config and scripts

--- start-mindie.sh
+++ start-mindie.sh
@@ -2,11 +2,11 @@
 
 ENV_DIR=$(dirname "$0")
 CONFIG_FILE=/usr/local/Ascend/mindie/latest/mindie-service/conf/config.json
-# 如果不存在.bak就创建一份
 if ! [ -f "$CONFIG_FILE.bak" ]; then
+    echo Backup config file
     cp "$CONFIG_FILE" "$CONFIG_FILE.bak"
 fi
-cp $ENV_DIR/mindie-config.json $CONFIG_FILE
+cp $ENV_DIR/mindie_config-FairyR1.json $CONFIG_FILE
 
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 source /usr/local/Ascend/nnal/atb/set_env.sh
