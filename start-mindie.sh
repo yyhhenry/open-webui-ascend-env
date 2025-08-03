edfca94abf4f5792c44a4d1489e9638eb78280af
yyhhenry <yyhhenry@foxmail.com>
2025-08-03 13:47:51 +0800

    chore: config changes

--- start-mindie.sh
+++ start-mindie.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 ENV_DIR=$(dirname "$0")
-cp $ENV_DIR/mindie-config.json /usr/local/Ascend/mindie/latest/mindie-service/conf/config.json
+CONFIG_FILE=/usr/local/Ascend/mindie/latest/mindie-service/conf/config.json
+# 如果不存在.bak就创建一份
+if ! [ -f "$CONFIG_FILE.bak" ]; then
+    cp "$CONFIG_FILE" "$CONFIG_FILE.bak"
+fi
+cp $ENV_DIR/mindie-config.json $CONFIG_FILE
 
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 source /usr/local/Ascend/nnal/atb/set_env.sh
